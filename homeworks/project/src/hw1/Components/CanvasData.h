6e5cec5128d6956e2ef2de6a19b01ce291b25bfb
linkun <787624675@qq.com>
2020-11-23 19:00:13 +0800

    doing hw04

--- homeworks/project/src/hw1/Components/CanvasData.h
+++ homeworks/project/src/hw1/Components/CanvasData.h
@@ -1,9 +1,24 @@
 #pragma once
 
 #include <UGM/UGM.h>
+struct Slope {
+	float l;
+	float r;
+};
+
+struct Ratio {
+	float l;
+	float r;
+	Ratio() { l = r = 1.f; }
+};
 
 struct CanvasData {
 	std::vector<Ubpa::pointf2> points;
+	std::vector<Ubpa::pointf2> ltangent;
+	std::vector<Ubpa::pointf2> rtangent;
+	std::vector<Ratio> tangent_ratio;
+	std::vector<Slope> xk;
+	std::vector<Slope> yk;
 	Ubpa::valf2 scrolling{ 0.f,0.f };
 	bool opt_enable_grid{ true };
 	bool opt_enable_context_menu{ true };
@@ -18,7 +33,44 @@ struct CanvasData {
 	bool showCruve7{ false };  // 7.Centripetal parameterization
 	bool showCruve8{ false };  // 8.Foley parameterization
 	bool cubic{ false };  // Creat editable cubic spline cruve
-	bool edit{ false };   // �༭ģʽ
+	bool hermit{ false };
+	bool edit{ false };   // edit mode
+	int edit_index = 1;
+
+
+	int fitting_type{ 0 };
+	bool enable_add_point{ true };
+	bool adding_last_point{ false };
+	int edit_point{ 0 };
+	int editing_index = -1;
+	bool enable_move_point{ false };
+	int editing_tan_index = 0; // < 0 is left, > 0 is right, real index = this - 1
+	bool enable_move_tan{ false };
+	void pop_back() {
+		points.pop_back();
+		ltangent.pop_back();
+		rtangent.pop_back();
+		xk.pop_back();
+		yk.pop_back();
+		tangent_ratio.pop_back();
+	}
+	void clear() {
+		points.clear();
+		ltangent.clear();
+		rtangent.clear();
+		xk.clear();
+		yk.clear();
+		tangent_ratio.clear();
+	}
+	void push_back(const Ubpa::pointf2& p) {
+		points.push_back(p);
+		ltangent.push_back(Ubpa::pointf2());
+		rtangent.push_back(Ubpa::pointf2());
+		xk.push_back(Slope());
+		yk.push_back(Slope());
+		tangent_ratio.push_back(Ratio());
+	}
+
 
 };
 
